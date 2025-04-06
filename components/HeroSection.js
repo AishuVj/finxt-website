c7ef68096eb93379979f7ad92f1fe45e78bb95ba
Iswarya Vijayakumar <ishuvijay@Iswaryas-MacBook-Pro.local>
2025-04-06 20:28:49 +0100

    mobile responsive hero image

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,11 +1,11 @@
 
 export default function HeroSection() {
   return (
-    <section className="">
+    <section className="px-4 pt-10 max-w-7xl mx-auto">
         <img
          src="/images/MainPicture.jpg"
          
-         className="w-full h-auto object-contain max-h-[300px] rounded-md shadow-md"
+         className="w-full object-cover h-[300px] md:h-[500px] lg:h-[600px] rounded-md shadow-md"
         />
    
     </section>
