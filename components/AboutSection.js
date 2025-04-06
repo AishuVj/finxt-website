2aa172834b366b3317a6a9a285f4adc020dd74f3
Iswarya Vijayakumar <ishuvijay@Iswaryas-MacBook-Pro.local>
2025-04-06 21:31:59 +0100

    Changing description in about section

--- components/AboutSection.js
+++ components/AboutSection.js
@@ -14,14 +14,21 @@ export default function AboutSection() {
         <p className="text-gray-600 max-w-6xl mx-auto text-justify px-4 mb-10">
         At FinXT, we harness the power of artificial intelligence to revolutionize your business, transforming challenges into opportunities. Specializing in strategic AI solutions, we're your trusted partner in driving growth, enhancing security, and delivering unparalleled customer experiences.
                         <h2 className="text-center text-4xl mb-4 mt-10" style={{color: '#2f6d6a'}}>Why Choose FinXT?</h2>
-              <div className="flex justify-center">
-                <ul className="list-disc  text-left mt-4 mb-4 space-y-2 px-4">
-                    <li>Cutting-edge AI technologies</li>
-                    <li>Personalized consulting and solutions</li>
-                    <li>Commitment to compliance and cybersecurity</li>
-                    <li> Proven expertise in industry-leading solutions</li>
-                </ul>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 px-4">
+            {[
+              'Cutting-edge AI Technologies',
+              'Personalized consulting and solutions',
+              'Commitment to compliance and Cyber-security',
+              'Proven expertise in industry-leading solutions'
+            ].map((item, idx) => (
+              <div
+                key={idx}
+                className="bg-gray-100 rounded-xl shadow-md p-6 text-center font-medium text-lg"
+              >
+                {item}
               </div>
+          ))}
+        </div>
               <p className="mt-4">Elevate your business today and embrace the future confidently with FinXT.</p>
 
         </p>
