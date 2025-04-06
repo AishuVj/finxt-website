00fbe7b823ab64adec2c79d93c9cfb6546b49c68
Iswarya Vijayakumar <ishuvijay@Iswaryas-MacBook-Pro.local>
2025-04-06 20:55:46 +0100

    Changing description in about section

--- components/AboutSection.js
+++ components/AboutSection.js
@@ -3,7 +3,7 @@ export default function AboutSection() {
   return (
     <section
       id="about"
-      className="py-16 px-6 mt-22"
+      className="py-16 px-6 mt-24"
       style={{ backgroundColor: '#f7f3ed' }} // Soft peach background
     >
       <div className="max-w-6xl mx-auto text-center">
@@ -13,12 +13,12 @@ export default function AboutSection() {
         </h2>
         <p className="text-gray-600 max-w-6xl mx-auto text-justify px-4 mb-10">
         At FinXT, we harness the power of artificial intelligence to revolutionize your business, transforming challenges into opportunities. Specializing in strategic AI solutions, we're your trusted partner in driving growth, enhancing security, and delivering unparalleled customer experiences.
-                        <h3>Why Choose FinXT?</h3>
-              <ul>
-                  <p>Cutting-edge AI technologies</p>
-                  <p>Personalized consulting and solutions</p>
-                  <p>Commitment to compliance and cybersecurity</p>
-                  <p> Proven expertise in industry-leading solutions</p>
+                        <h3 className="text-center"style={{color: '#2f6d6a'}}>Why Choose FinXT?</h3>
+              <ul className="list-disc list-inside text-center mt-4">
+                  <li>Cutting-edge AI technologies</li>
+                  <li>Personalized consulting and solutions</li>
+                  <li>Commitment to compliance and cybersecurity</li>
+                  <li> Proven expertise in industry-leading solutions</li>
               </ul>
               <p>Elevate your business today and embrace the future confidently with FinXT.</p>
 
