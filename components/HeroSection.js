b1c0337030b251c302adaa8d58786babd1f4a1fa
Iswarya Vijayakumar <ishuvijay@Iswaryas-MacBook-Pro.local>
2025-04-06 20:24:30 +0100

    mobile responsive hero image

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,7 +1,7 @@
 
 export default function HeroSection() {
   return (
-    <section className="">
+    <section className="px-5 pt-5">
         <img
          src="/images/MainPicture.jpg"
          
